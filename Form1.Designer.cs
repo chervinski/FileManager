862f69bba0cf8cbb4fb36bc60a20b6d311550afb
Max <unfusions@gmail.com>
2020-09-27 20:30:34 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace FileManager
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.listBox1 = new System.Windows.Forms.ListBox();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+			this.toolsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.propertiesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.refreshToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.viewToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.fontToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.backToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.button1 = new System.Windows.Forms.Button();
+			this.menuStrip1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// listBox1
+			// 
+			this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.listBox1.FormattingEnabled = true;
+			this.listBox1.HorizontalScrollbar = true;
+			this.listBox1.ItemHeight = 18;
+			this.listBox1.Location = new System.Drawing.Point(12, 52);
+			this.listBox1.Name = "listBox1";
+			this.listBox1.Size = new System.Drawing.Size(556, 382);
+			this.listBox1.TabIndex = 0;
+			this.listBox1.DoubleClick += new System.EventHandler(this.listBox1_DoubleClick);
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(64, 27);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(504, 20);
+			this.textBox1.TabIndex = 1;
+			this.textBox1.Enter += new System.EventHandler(this.textBox1_Enter);
+			this.textBox1.KeyUp += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyUp);
+			this.textBox1.Leave += new System.EventHandler(this.textBox1_Leave);
+			// 
+			// menuStrip1
+			// 
+			this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolsToolStripMenuItem,
+            this.viewToolStripMenuItem});
+			this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+			this.menuStrip1.Name = "menuStrip1";
+			this.menuStrip1.Size = new System.Drawing.Size(580, 24);
+			this.menuStrip1.TabIndex = 2;
+			this.menuStrip1.Text = "menuStrip1";
+			// 
+			// toolsToolStripMenuItem
+			// 
+			this.toolsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.propertiesToolStripMenuItem,
+            this.refreshToolStripMenuItem});
+			this.toolsToolStripMenuItem.Name = "toolsToolStripMenuItem";
+			this.toolsToolStripMenuItem.Size = new System.Drawing.Size(46, 20);
+			this.toolsToolStripMenuItem.Text = "Tools";
+			this.toolsToolStripMenuItem.DropDownOpened += new System.EventHandler(this.Tools_Popup);
+			// 
+			// propertiesToolStripMenuItem
+			// 
+			this.propertiesToolStripMenuItem.Name = "propertiesToolStripMenuItem";
+			this.propertiesToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Alt | System.Windows.Forms.Keys.P)));
+			this.propertiesToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+			this.propertiesToolStripMenuItem.Text = "Properties";
+			this.propertiesToolStripMenuItem.Click += new System.EventHandler(this.Properties_Click);
+			// 
+			// refreshToolStripMenuItem
+			// 
+			this.refreshToolStripMenuItem.Name = "refreshToolStripMenuItem";
+			this.refreshToolStripMenuItem.ShortcutKeys = System.Windows.Forms.Keys.F5;
+			this.refreshToolStripMenuItem.Size = new System.Drawing.Size(132, 22);
+			this.refreshToolStripMenuItem.Text = "Refresh";
+			// 
+			// viewToolStripMenuItem
+			// 
+			this.viewToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fontToolStripMenuItem,
+            this.backToolStripMenuItem});
+			this.viewToolStripMenuItem.Name = "viewToolStripMenuItem";
+			this.viewToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+			this.viewToolStripMenuItem.Text = "View";
+			// 
+			// fontToolStripMenuItem
+			// 
+			this.fontToolStripMenuItem.Name = "fontToolStripMenuItem";
+			this.fontToolStripMenuItem.Size = new System.Drawing.Size(99, 22);
+			this.fontToolStripMenuItem.Text = "Font";
+			this.fontToolStripMenuItem.Click += new System.EventHandler(this.Font_Click);
+			// 
+			// backToolStripMenuItem
+			// 
+			this.backToolStripMenuItem.Name = "backToolStripMenuItem";
+			this.backToolStripMenuItem.Size = new System.Drawing.Size(99, 22);
+			this.backToolStripMenuItem.Text = "Back";
+			this.backToolStripMenuItem.Click += new System.EventHandler(this.Back_Click);
+			// 
+			// button1
+			// 
+			this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.button1.Location = new System.Drawing.Point(12, 27);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(46, 20);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "↑";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(580, 445);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.listBox1);
+			this.Controls.Add(this.menuStrip1);
+			this.MainMenuStrip = this.menuStrip1;
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.SizeChanged += new System.EventHandler(this.Form1_SizeChanged);
+			this.menuStrip1.ResumeLayout(false);
+			this.menuStrip1.PerformLayout();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.ListBox listBox1;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.MenuStrip menuStrip1;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.ToolStripMenuItem toolsToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem refreshToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem viewToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem fontToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem backToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem propertiesToolStripMenuItem;
+	}
+}
---
+++ Form1.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Drawing;
+using System.Reflection;
+using System.Collections;
+using System.Windows.Forms;
+
+namespace FileManager
+{
+	public partial class Form1 : Form
+	{
+		private string saved;
+		public Form1()
+		{
+			InitializeComponent();
+			UpdateList();
+			listBox1.ContextMenu = new ContextMenu() {
+				MenuItems = {
+					new MenuItem("Properties", Properties_Click),
+					new MenuItem("Refresh", (s, e) => UpdateList())
+				}
+			};
+			listBox1.ContextMenu.Popup += Tools_Popup;
+			refreshToolStripMenuItem.Click += (s, e) => UpdateList();
+		}
+		private void textBox1_KeyUp(object sender, KeyEventArgs e)
+		{
+			if (e.KeyCode == Keys.Enter)
+				listBox1.Focus();
+		}
+		private void textBox1_Enter(object sender, EventArgs e)
+		{
+			saved = textBox1.Text;
+			button1.Enabled = false;
+		}
+		private void textBox1_Leave(object sender, EventArgs e)
+		{
+			if (textBox1.Text != String.Empty && !Directory.Exists(textBox1.Text))
+			{
+				MessageBox.Show("Could not found this directory.");
+				textBox1.Text = saved;
+			}
+			saved = null;
+			UpdateList();
+		}
+
+		private void UpdateList()
+		{
+			button1.Enabled = textBox1.Text != String.Empty;
+
+			listBox1.Items.Clear();
+			if (textBox1.Text == String.Empty)
+			{
+				foreach (DriveInfo driver in DriveInfo.GetDrives())
+					if (driver.DriveType == DriveType.Fixed)
+						listBox1.Items.Add(driver.Name);
+			}
+			else
+			{
+				foreach (string file in Directory.EnumerateFileSystemEntries(textBox1.Text))
+					listBox1.Items.Add(Path.GetFileName(file));
+			}
+		}
+		private void listBox1_DoubleClick(object sender, EventArgs e)
+		{
+			if (listBox1.SelectedIndex != -1)
+			{
+				string newFile = Path.Combine(textBox1.Text, listBox1.SelectedItem as string);
+				if (File.GetAttributes(newFile).HasFlag(FileAttributes.Directory))
+				{
+					textBox1.Text = newFile;
+					UpdateList();
+				}
+				else MessageBox.Show("This is a file");
+			}
+		}
+		private void button1_Click(object sender, EventArgs e)
+		{
+			string parent = Path.GetFullPath(Path.Combine(textBox1.Text, ".."));
+			textBox1.Text = textBox1.Text == parent ? String.Empty : parent;
+			UpdateList();
+		}
+
+		private void Form1_SizeChanged(object sender, EventArgs e)
+		{
+			textBox1.Width = ClientSize.Width - textBox1.Location.X - button1.Location.X;
+			listBox1.Size = new Size(
+				ClientSize.Width - listBox1.Location.X - listBox1.Location.X,
+				ClientSize.Height - listBox1.Location.Y - listBox1.Location.X);
+		}
+		private void Tools_Popup(object sender, EventArgs e)
+		{
+			IEnumerable items = sender is ContextMenu ?
+				(sender as ContextMenu).MenuItems as IEnumerable :
+				(sender as ToolStripMenuItem).DropDownItems as IEnumerable;
+
+			IEnumerator item = items.GetEnumerator();
+			item.Reset();
+			item.MoveNext();
+			PropertyInfo enabled = item.Current.GetType().GetProperty("Enabled");
+			enabled.SetValue(item.Current, listBox1.SelectedIndex != -1);
+		}
+		private void Properties_Click(object sender, EventArgs e)
+		{
+			if (listBox1.SelectedIndex == -1) return;
+
+			StringBuilder sb = new StringBuilder(150);
+			string fullPath = Path.Combine(textBox1.Text, listBox1.SelectedItem as string);
+			bool isDirecory = File.GetAttributes(fullPath).HasFlag(FileAttributes.Directory);
+			FileSystemInfo info = isDirecory ? new DirectoryInfo(fullPath) as FileSystemInfo : new FileInfo(fullPath) as FileSystemInfo;
+
+			sb.Append("Type:\t\t");
+			sb.Append(isDirecory ? "Directory" : "File");
+			sb.Append(Environment.NewLine);
+
+			if (!isDirecory)
+			{
+				sb.Append("Size:\t\t");
+				sb.Append(FormatSize((info as FileInfo).Length));
+				sb.Append(Environment.NewLine);
+			}
+
+			sb.Append("Creation time:\t");
+			sb.Append(info.CreationTime.ToString());
+			sb.Append(Environment.NewLine);
+
+			sb.Append("Last write time:\t");
+			sb.Append(info.LastWriteTime.ToString());
+			sb.Append(Environment.NewLine);
+
+			sb.Append("Last access time:\t");
+			sb.Append(info.LastAccessTime.ToString());
+			MessageBox.Show(sb.ToString());
+		}
+
+		private void Font_Click(object sender, EventArgs e)
+		{
+			FontDialog dialog = new FontDialog();
+			dialog.ShowColor = true;
+			dialog.Font = listBox1.Font;
+			dialog.Color = listBox1.ForeColor;
+			if (dialog.ShowDialog() == DialogResult.OK)
+			{
+				listBox1.Font = dialog.Font;
+				listBox1.ForeColor = dialog.Color;
+			}
+		}
+		private void Back_Click(object sender, EventArgs e)
+		{
+			ColorDialog dialog = new ColorDialog();
+			dialog.Color = listBox1.BackColor;
+			if (dialog.ShowDialog() == DialogResult.OK)
+				listBox1.BackColor = dialog.Color;
+		}
+
+		private static readonly string[] suffixes = { "B", "KB", "MB", "GB", "TB", "PB" };
+		public static string FormatSize(long bytes)
+		{
+			int counter = 0;
+			double number = bytes;
+			while (Math.Round(number / 1024) >= 1)
+			{
+				number = number / 1024;
+				++counter;
+			}
+			return string.Format("{0:F2} {1}", number, suffixes[counter]);
+		}
+	}
+}
